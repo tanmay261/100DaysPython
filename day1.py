2655e73227b75412af2c3f72baf77328be86251e
Tanmay <tanmaytanu.10@gmail.com>
2022-12-29 19:04:03 +0530

    Day 1

--- day1.py
+++ day1.py
@@ -2,3 +2,8 @@ print("Greetings \nWhats your city name")
 city=input()
 pet=input("Whats your pet's name ")
 print("Band name can be "+ city + " "+ pet)
+
+# Learnings
+# 1. Printing and taking input using a single statement
+
+print("Learnt printing and taking input " + input("Enter input ") + " using a single statement")
